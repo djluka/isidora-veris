8f4825260027555bd475796c6cf616d46457ce5c
djluka <66550706+djluka@users.noreply.github.com>
2025-03-31 02:29:39 +0200

    animation fix 1

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const observer = new IntersectionObserver((entries) =>{
     entries.forEach((entry)=>{
-        console.log(entry)
         if (entry.isIntersecting){
             entry.target.classList.add('show')
         }
@@ -8,3 +7,17 @@ const observer = new IntersectionObserver((entries) =>{
 });
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((e)=> observer.observe(e));
+
+
+const clients = document.querySelectorAll('.single-klijenti');
+let start = 0.2;  
+
+clients.forEach((client) => {
+    client.style.transitionDelay = `${start}s`;
+
+    start += 0.2;
+
+    if (start > 0.5) {
+        start = 0.2;
+    }
+});
