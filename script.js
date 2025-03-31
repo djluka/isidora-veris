29944bc81dfcce09ce8993a5efa5056b1aae22ef
djluka <66550706+djluka@users.noreply.github.com>
2025-03-31 02:50:24 +0200

    animation final touch

--- script.js
+++ script.js
@@ -6,18 +6,24 @@ const observer = new IntersectionObserver((entries) =>{
     })
 });
 const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements1 = document.querySelectorAll('.hidden1');
+
 hiddenElements.forEach((e)=> observer.observe(e));
+hiddenElements1.forEach((e)=> observer.observe(e));
 
 
-const clients = document.querySelectorAll('.single-klijenti');
+
+let boxes = document.querySelectorAll('.animationBox');
 let start = 0.2;  
 
-clients.forEach((client) => {
-    client.style.transitionDelay = `${start}s`;
+boxes.forEach((boxes) => {
+    boxes.style.transitionDelay = `${start}s`;
 
     start += 0.2;
 
-    if (start > 0.5) {
+    if (start > 0.7) {
         start = 0.2;
     }
+    console.log(boxes)
+
 });
